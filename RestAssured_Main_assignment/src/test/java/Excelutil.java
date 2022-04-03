2118350dcd5decc176e9654bba605acba6369cfc
prabhat-deloitte <praparihar@deloitte.com>
2022-04-03 22:37:58 +0530

    Enabling data driven Testcases using Excel

---
+++ RestAssured_Main_assignment/src/test/java/Excelutil.java
@@ -0,0 +1,206 @@
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.json.JSONObject;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Excelutil {
+
+    static int Sheet_no ;
+    static String name = null;
+    static String email_id = null;
+    static String password = null;
+    static String age = null;
+    static String token = null;
+    static String user_id = null;
+    public static org.json.simple.JSONObject Get_Data(int i, int value){
+        String Excel_file_path = ("C:\\Users\\praparihar\\Desktop\\Database.xlsx");
+        try {
+            FileInputStream fis = new FileInputStream(Excel_file_path);
+            XSSFWorkbook workbook = new XSSFWorkbook(fis);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            XSSFRow row = null;
+            XSSFCell cell = null;
+                row = sheet.getRow(i);
+                for (int j =0; j< row.getLastCellNum(); j++){
+                    cell = row.getCell(j);
+                    if (j==0){
+                        name = cell.getStringCellValue();
+                    }
+                    if (j==1){
+                        email_id = cell.getStringCellValue();
+                    }
+                    if (j==2){
+                        password = cell.getStringCellValue();
+                    }
+                    if (j==3){
+                       age  = cell.getRawValue();
+                    }
+                    if (j==4){
+                        token = cell.getStringCellValue();
+                    }
+                    if (j==5){
+                        user_id = cell.getStringCellValue();
+                    }
+
+                }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (value == 1){
+            return Create_user_json(name,email_id,password,age);
+        }
+        if (value == 2){
+            return Login_json(email_id,password);
+        }
+        return Create_user_json(name,email_id,password,age);
+}
+
+  public  static  org.json.simple.JSONObject Create_user_json(String Name,String Email,String Pass,String Age){
+        org.json.simple.JSONObject obj = new org.json.simple.JSONObject();
+        obj.put("name",Name);
+        obj.put("email",Email);
+        obj.put("password",Pass);
+        obj.put("age",Age);
+        return obj;
+    }
+    public  static  org.json.simple.JSONObject Login_json(String Email,String Pass){
+        org.json.simple.JSONObject obj = new org.json.simple.JSONObject();
+
+        obj.put("email",Email);
+        obj.put("password",Pass);
+
+        return obj;
+    }
+    public  static  org.json.simple.JSONObject Task_json(String Description){
+        org.json.simple.JSONObject obj = new org.json.simple.JSONObject();
+
+        obj.put("description",Description);
+
+        return obj;
+    }
+
+
+    public static int Row_no(int value){
+
+        String Excel_file_path = ("C:\\Users\\praparihar\\Desktop\\Database.xlsx");
+        try {
+            FileInputStream fis = new FileInputStream(Excel_file_path);
+            XSSFWorkbook workbook = new XSSFWorkbook(fis);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            XSSFSheet sheet1 = workbook.getSheetAt(1);
+            Sheet_no = sheet.getLastRowNum();
+           int Sheet2_no = sheet1.getLastRowNum();
+           if (value == 1){
+               return Sheet2_no;
+           }
+            if (value == 0){
+                return Sheet_no;
+            }
+    }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();}
+        return Sheet_no;
+}
+       public static void Write_data_token(String Token, int i) {
+           String Excel_file_path = ("C:\\Users\\praparihar\\Desktop\\Database.xlsx");
+           try {
+               FileInputStream fis = new FileInputStream(Excel_file_path);
+               XSSFWorkbook workbook = new XSSFWorkbook(fis);
+               XSSFSheet sheet = workbook.getSheetAt(0);
+               XSSFRow row = null;
+               XSSFCell cell = null;
+               row = sheet.getRow(i);
+
+                       System.out.println("token is printing");
+                       cell = row.createCell(4);
+                       cell.setCellValue(Token);
+                       fis.close();
+                     FileOutputStream fos = new FileOutputStream(Excel_file_path);
+                     workbook.write(fos);
+                     //fos.flush();
+                     fos.close();
+           } catch (FileNotFoundException e) {
+               e.printStackTrace();
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+       }
+
+    public static void Write_data_id(String id, int i) {
+        String Excel_file_path = ("C:\\Users\\praparihar\\Desktop\\Database.xlsx");
+        try {
+            FileInputStream fis = new FileInputStream(Excel_file_path);
+            XSSFWorkbook workbook = new XSSFWorkbook(fis);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            XSSFRow row = null;
+            XSSFCell cell = null;
+            row = sheet.getRow(i);
+
+            System.out.println("Writing in Excel");
+            cell = row.createCell(5);
+            cell.setCellValue(id);
+            fis.close();
+            FileOutputStream fos = new FileOutputStream(Excel_file_path);
+            workbook.write(fos);
+            //fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public String GET_Token(int i){
+        String Excel_file_path = ("C:\\Users\\praparihar\\Desktop\\Database.xlsx");
+        try {
+            FileInputStream fis = new FileInputStream(Excel_file_path);
+            XSSFWorkbook workbook = new XSSFWorkbook(fis);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            XSSFRow row = null;
+            XSSFCell cell = null;
+            row = sheet.getRow(i);
+            cell = row.getCell(4);
+            return cell.getStringCellValue();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }return "unable to fetch";
+
+    }
+    public org.json.simple.JSONObject GET_Task(int i){
+        String Excel_file_path = ("C:\\Users\\praparihar\\Desktop\\Database.xlsx");
+        try {
+            FileInputStream fis = new FileInputStream(Excel_file_path);
+            XSSFWorkbook workbook = new XSSFWorkbook(fis);
+            XSSFSheet sheet = workbook.getSheetAt(1);
+            XSSFRow row = null;
+            XSSFCell cell = null;
+            row = sheet.getRow(i);
+            cell = row.getCell(0);
+            return Task_json(cell.getStringCellValue());
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }return Task_json("unable_to_fetch");
+
+    }
+    }
+
+
+
--- RestAssured_Main_assignment/src/test/java/Main_test.java
+++ RestAssured_Main_assignment/src/test/java/Main_test.java
@@ -1,151 +1,127 @@
 import io.restassured.response.Response;
-import io.restassured.response.ResponseBodyExtractionOptions;
-import org.apache.poi.ss.formula.functions.T;
-import org.json.JSONArray;
 import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.testng.annotations.Test;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.testng.AssertJUnit.assertTrue;
-
 public class Main_test {
     String Token;
     String Email_data_base;
+    Excelutil excel  = new Excelutil();
     public String Token_PArse(Response res){
         return res.jsonPath().getString("token");
     }
 
     @Test(priority = 1)
     public void create_user(){
+      for (int i=1; i<=excel.Row_no(0);i++) {
+          org.json.simple.JSONObject jd = excel.Get_Data(i,1);
+          Response response = given().baseUri("https://api-nodejs-todolist.herokuapp.com/user/register").
+                  header("content-type", "application/json").
+                  body(jd.toJSONString()).
+                  when().
+                  post().
+                  then().extract().response();
+          Token = Token_PArse(response);
+          //System.out.println(Token);
+          excel.Write_data_token(Token, i);
 
-        File jd= new File("C:\\Users\\praparihar\\IdeaProjects\\Mini_assingment2_Api_Testing\\RestAssured_Main_assignment\\src\\test\\java\\Register.json");
-        Response response = given().baseUri("https://api-nodejs-todolist.herokuapp.com/user/register").
-                header("content-type","application/json").
-                body(jd).
-                when().
-                post().
-                then().extract().response();
-        Token = Token_PArse(response);
-        System.out.println(Token);
+
         //validate
         JSONObject arr = new JSONObject(response.asString());
         String Email_resp = (String)arr.getJSONObject("user").get("email");
-        JSONParser jsonParser = new JSONParser();
 
-        try (FileReader reader = new FileReader("C:\\Users\\praparihar\\IdeaProjects\\Mini_assingment2_Api_Testing\\RestAssured_Main_assignment\\src\\test\\java\\Register.json"))
-        {
-            //Read JSON file
-            Object obj = jsonParser.parse(reader);
-            org.json.simple.JSONObject jsonObject = (org.json.simple.JSONObject)obj;
-            Email_data_base = (String)jsonObject.get("email");
-
-
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        if (Email_resp.equalsIgnoreCase(Email_data_base)){
+        if (Email_resp.equalsIgnoreCase(excel.email_id)){
             assertTrue(true);
         }
         else{
-            System.out.println(Email_resp + Email_data_base);
+
             assertTrue(false);
-        }
+        }}
     }
 
     @Test(priority = 2)
-    public void Login(){File jd= new File("C:\\Users\\praparihar\\IdeaProjects\\Mini_assingment2_Api_Testing\\RestAssured_Main_assignment\\src\\test\\java\\login.json");
+    public void Login(){
+        for (int i=1; i<=excel.Row_no(0);i++) {
+            org.json.simple.JSONObject jd = excel.Get_Data(i,2);
+
         Response response = given().baseUri("https://api-nodejs-todolist.herokuapp.com/user/login").
                 header("content-type","application/json").
-                body(jd).
+                body(jd.toJSONString()).
                 when().
                 post().
                 then().extract().response();
         //validate
         JSONObject arr = new JSONObject(response.asString());
         String Email_resp = (String)arr.getJSONObject("user").get("email");
-        JSONParser jsonParser = new JSONParser();
-
-        try (FileReader reader = new FileReader("C:\\Users\\praparihar\\IdeaProjects\\Mini_assingment2_Api_Testing\\RestAssured_Main_assignment\\src\\test\\java\\login.json"))
-        {
-            //Read JSON file
-            Object obj = jsonParser.parse(reader);
-            org.json.simple.JSONObject jsonObject = (org.json.simple.JSONObject)obj;
-            Email_data_base = (String)jsonObject.get("email");
-
-
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        if (Email_resp.equalsIgnoreCase(Email_data_base)){
+        String User_id = (String)arr.getJSONObject("user").get("_id");
+        excel.Write_data_id(User_id, i);
+        if (Email_resp.equalsIgnoreCase(excel.email_id)){
+            System.out.println(jd+"    ----> Validated");
             assertTrue(true);
         }
         else{
-            System.out.println(Email_resp + Email_data_base);
+
             assertTrue(false);
-        }
+        }}
 
     }
 
     @Test(priority = 3)
     public void Creating_Tasks(){
-
-        File Task = new File("C:\\Users\\praparihar\\IdeaProjects\\Mini_assingment2_Api_Testing\\RestAssured_Main_assignment\\src\\test\\java\\CreateTask.json");
-        String temp_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjQ5NTMzNzE2YTc0ZTAwMTc0NWZlZWEiLCJpYXQiOjE2NDg5NzI1OTl9.m1zS_tE3LXb4EXQ-lb31Fx7INfsvziTOiGEqTDCCnGc";
+        for (int i=1; i<=excel.Row_no(0);i++) {
+            String temp_token = excel.GET_Token(i);
+            for (int j=1; j<=excel.Row_no(1);j++) {
+               org.json.simple.JSONObject Task = excel.GET_Task(j);
+        //File Task = new File("C:\\Users\\praparihar\\IdeaProjects\\Mini_assingment2_Api_Testing\\RestAssured_Main_assignment\\src\\test\\java\\CreateTask.json");
+        //String temp_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjQ5NTMzNzE2YTc0ZTAwMTc0NWZlZWEiLCJpYXQiOjE2NDg5NzI1OTl9.m1zS_tE3LXb4EXQ-lb31Fx7INfsvziTOiGEqTDCCnGc";
         given().baseUri("https://api-nodejs-todolist.herokuapp.com/task").
                     header("Authorization", "Bearer " + temp_token).header("content-type","application/json").
-                    body(Task)
+                    body(Task.toJSONString())
                     .when()
                     .post()
                     .then()
                     .statusCode(201);
-        }
-        @Test
+        }}}
+        @Test(priority = 4)
     public void Validating_paging(){
+        for (int i=1; i<=excel.Row_no(0);i++) {
+            try{int num = 2;
+                for(int j = 0; j<3; j++){
+                String temp_token = excel.GET_Token(i);
 
-        try{
-        String temp_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjQ5NTMzNzE2YTc0ZTAwMTc0NWZlZWEiLCJpYXQiOjE2NDg5NzI1OTl9.m1zS_tE3LXb4EXQ-lb31Fx7INfsvziTOiGEqTDCCnGc";
-        int num = 2;
-       Response response = given().baseUri("https://api-nodejs-todolist.herokuapp.com/task?limit=2").
-                header("Authorization", "Bearer " + temp_token).header("content-type","application/json")
-                .when()
-                .get()
-                .then()
-                .statusCode(200).extract().response();
-       JSONObject arr = new JSONObject(response.asString());
-       int Count = (int)arr.get("count");
-       if (num == Count){
-           assertTrue(true);
+                Response response = given().baseUri("https://api-nodejs-todolist.herokuapp.com/task").
+                        header("Authorization", "Bearer " + temp_token).header("content-type","application/json")
+                        .when()
+                        .get("?limit="+num)
+                        .then()
+                        .statusCode(200).extract().response();
+                JSONObject arr = new JSONObject(response.asString());
+                int Count = (int)arr.get("count");
+                if (num == Count && num==2){
+                    System.out.println("assertion of paging  for 2 is successful");
+                    num = num+3;
 
-       }}catch (Exception e){
-            System.out.println(e);
+                }
+                if (num == Count && num==5){
+                    System.out.println("assertion of paging  for 5 is successful");
+                    num = num+5;
+
+                }
+                if (num== Count && num==10) {
+                    System.out.println("assertion of paging  for 10 is successful");
+                    assertTrue(true);
+                }
+            }}catch (Exception e){
+                System.out.println(e);}
+
+
+            }
         }
-
-        }
-
+}
 
 
-    }
 
 
 
